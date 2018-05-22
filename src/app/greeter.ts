981d4d47954e069be6a216e1c026304e8d95f56f
Ravishankargupta1112 <39280455+Ravishankargupta1112@users.noreply.github.com>
2018-05-22 07:26:44 +0530

    Add files via upload

---
+++ src/app/greeter.ts
@@ -0,0 +1,17 @@
+class Greeter {
+    greeting: string;
+    finalMessage: String;
+    constructor(message: string) {
+        this.greeting = message;
+    }
+    greet() {
+        this.finalMessage = "Hello, this is " + this.greeting;
+        alert(this.finalMessage);
+    }
+}
+
+window.onload = () =>
+{
+    let greeter = new Greeter("Ravi Shankar Gupta");
+    greeter.greet();  
+};
